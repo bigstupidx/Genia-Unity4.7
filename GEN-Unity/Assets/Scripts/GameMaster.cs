071aec67dceeb2d41bb729f702ae48fb1181c83f
sirerr <sirerr@gmail.com>
2014-02-13 22:08:25 -0500

    Everything

---
+++ GEN-Unity/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	private GameObject mainplayer;
+	private GameObject sucker;
+
+	// Use this for initialization
+	void Start () {
+		sucker = GameObject.Find("sucker");
+		sucker.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		bool startsucker = Input.GetKeyDown(KeyCode.Space);
+
+		if(startsucker){
+			if(!sucker.activeSelf)
+			{
+				sucker.SetActive(true);
+			}
+		}
+
+
+	}
+}
---
+++ GEN-Unity/Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public GUIText leveltimer;
+	public float timercount;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+
+}
