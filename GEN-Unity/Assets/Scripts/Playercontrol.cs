9abc9a92eee815d18d1f2a474e635f78c74da112
sirerr <sirerr@gmail.com>
2014-03-24 22:14:00 -0400

    new script to move

---
+++ GEN-Unity/Assets/Scripts/Playercontrol.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercontrol : MonoBehaviour {
+
+	public float playerspeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(Screen.width-200,Screen.height -300,50,50),"^"))
+		{
+			transform.Translate(Vector3.forward *playerspeed*Time.deltaTime);
+			print("go up");
+		}
+		if(GUI.Button(new Rect(Screen.width-200,Screen.height -225,50,50),"v"))
+		{
+			transform.Translate(Vector3.back *playerspeed*Time.deltaTime);
+			print("go down");
+		}
+
+	}
+}
--- GEN-Unity/Assets/Scripts/allbutton.cs
+++ GEN-Unity/Assets/Scripts/allbutton.cs
@@ -3,21 +3,17 @@ using System.Collections;
 
 public class allbutton : MonoBehaviour {
 	private GameObject weaponchoice;
-	public float speed;
-	public float hor;
-	public float ver;
+
 	private GameObject theplayer;
-	public float playerspeed = 0;
+
 
 	// Use this for initialization
 	void Start () {
-		hor  = Input.GetAxis("Horizontal");
-		ver = Input.GetAxis("Vertical");
+	
 
 		weaponchoice = GameObject.Find("sucker");
 		weaponchoice.SetActive(false);
 
-		theplayer = GameObject.Find("Player");
 
 	 	}
 	
@@ -27,20 +23,6 @@ public class allbutton : MonoBehaviour {
 	}
 
 	void OnGUI () {
-
-			if(GUI.Button(new Rect(Screen.width-200,Screen.height -300,50,50),"^"))
-		              {
-			theplayer.transform.Translate(Vector3.forward *speed*Time.deltaTime *ver);
-
-			print("go up");
-			}
-			if(GUI.Button(new Rect(Screen.width-200,Screen.height -225,50,50),"v"))
-			{
-			theplayer.transform.Translate(Vector3.back *speed*Time.deltaTime*ver);
-			print("go down");
-			}
-
-
 
 			if (GUI.Button (new Rect (100,Screen.height -100,150,20), "Suck'em")) {
 			if( !weaponchoice.activeSelf)
