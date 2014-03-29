2d9cdadab78a1727384aebe0ea756c1eed8ae83e
sirerr <sirerr@gmail.com>
2014-03-29 18:19:00 -0400

    more work

--- GEN-Unity/Assets/Scripts/Playercontrol.cs
+++ GEN-Unity/Assets/Scripts/Playercontrol.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class Playercontrol : MonoBehaviour {
 
 	public float playerspeed;
+	private bool goup;
+	private bool godown;
+	private bool goleft;
+	private bool goright;
 
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -18,12 +22,14 @@ public class Playercontrol : MonoBehaviour {
 
 	void OnGUI()
 	{
-		if(GUI.Button(new Rect(Screen.width-200,Screen.height -300,50,50),"^"))
+		if(GUI.RepeatButton(new Rect(Screen.width-200,Screen.height -300,50,50),"^"))
 		{
 			transform.Translate(Vector3.forward *playerspeed*Time.deltaTime);
+			goup = true;
 			print("go up");
 		}
-		if(GUI.Button(new Rect(Screen.width-200,Screen.height -225,50,50),"v"))
+	
+		if(GUI.RepeatButton(new Rect(Screen.width-200,Screen.height -225,50,50),"v"))
 		{
 			transform.Translate(Vector3.back *playerspeed*Time.deltaTime);
 			print("go down");
