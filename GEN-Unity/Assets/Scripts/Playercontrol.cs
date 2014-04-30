d1b1079092fc30cd7f6c442dca32a4767c9b986a
sirerr <sirerr@gmail.com>
2014-04-29 21:15:22 -0400

    upload new apk

--- GEN-Unity/Assets/Scripts/Playercontrol.cs
+++ GEN-Unity/Assets/Scripts/Playercontrol.cs
@@ -13,6 +13,12 @@ public class Playercontrol : MonoBehaviour {
 	//public vector3 largerscale;
 	public float scalesize;
 	public static bool makelarger = false;
+
+	private Rect goingup;
+	private Rect goingdown;
+	private Rect goingleft;
+	private Rect goingright;
+	private GUIStyle movementstyle;
 	
 	//make larger scale 15
 	
@@ -41,8 +47,41 @@ public class Playercontrol : MonoBehaviour {
 		godown = false;
 		goleft =false;
 		goright =false;
+
+		goingup = new Rect(Screen.width-200,Screen.height -350,100,100);
+		goingdown = new Rect(Screen.width-200,Screen.height -150,100,100);
+		goingleft = new Rect(Screen.width-300,Screen.height -250,100,100);
+		goingright = new Rect(Screen.width-100,Screen.height -250,100,100);
+
+
+
 	}
-	
+
+	void Update()
+	{
+
+
+		for(int i =0; i<Input.touchCount;i++)
+		{
+			goup = goingup.Contains(Input.touches[i].position);
+			godown = goingdown.Contains(Input.touches[i].position);
+			goleft = goingleft.Contains(Input.touches[i].position);
+			goright = goingright.Contains(Input.touches[i].position);
+		}
+		if(Input.GetMouseButton(0))
+
+		{
+			Vector2 mousePosition = Input.mousePosition;
+			mousePosition.y = Screen.height - mousePosition.y;
+			goup = goingup.Contains(mousePosition);
+			godown = goingdown.Contains(mousePosition);
+			goleft = goingleft.Contains(mousePosition);
+			goright = goingright.Contains(mousePosition);
+		}
+
+
+	}
+
 	// Update is called once per frame
 	void FixedUpdate() {
 	
@@ -73,42 +112,49 @@ public class Playercontrol : MonoBehaviour {
 
 	void OnGUI()
 	{
-		if(GUI.RepeatButton(new Rect(Screen.width-200,Screen.height -350,100,100),"^"))
+		if(movementstyle ==null)
 		{
-			goup = true;
-		//	print("goup is true");
-		}else{
-		//	print("goup is false");
-			goup =false;
+			movementstyle = new GUIStyle("button");
 		}
+
+
+		GUI.Box(goingup,"^",movementstyle);
+
+		GUI.Box(goingdown,"v",movementstyle);
+
+		GUI.Box(goingleft,"<",movementstyle);
+
+		GUI.Box(goingright,">",movementstyle);
 	
-		if(GUI.RepeatButton(new Rect(Screen.width-200,Screen.height -150,100,100),"v"))
-		{
-			godown =true;
-		//	print("godown is true");
-		}else{
-		//	print("godown is false");
-			godown =false;
-		}
-
-		if(GUI.RepeatButton(new Rect(Screen.width-300,Screen.height -250,100,100),"<"))
-		{
-			goleft =true;
-		//	print("godown is true");
-		}else{
-		//	print("godown is false");
-			goleft =false;
-		}
-
-		if(GUI.Box.RepeatButton(new Rect(Screen.width-100,Screen.height -250,100,100),">"))
-		{
-			goright =true;
-		//	print("godown is true");
-		}else{
-		//	print("godown is false");
-			goright =false;
-		}
-		// save the rectangle to variables. touch input
+//	
+//		if(GUI.RepeatButton(new Rect(Screen.width-200,Screen.height -150,100,100),"v"))
+//		{
+//			godown =true;
+//		//	print("godown is true");
+//		}else{
+//		//	print("godown is false");
+//			godown =false;
+//		}
+//
+//		if(GUI.RepeatButton(new Rect(Screen.width-300,Screen.height -250,100,100),"<"))
+//		{
+//			goleft =true;
+//		//	print("godown is true");
+//		}else{
+//		//	print("godown is false");
+//			goleft =false;
+//		}
+//
+//		if(GUI.RepeatButton(new Rect(Screen.width-100,Screen.height -250,100,100),">"))
+//		{
+//			goright =true;
+//		//	print("godown is true");
+//		}else{
+//		//	print("godown is false");
+//			goright =false;
+//		}
+//		// save the rectangle to variables. touch input
+//
 
 	}
 }
