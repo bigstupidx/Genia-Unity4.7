e67b09c72c45f3f263da54bb6e4422ca4c8b5e2e
sirerr <sirerr@gmail.com>
2014-10-19 03:57:41 -0400

    get up to date

---
+++ GEN-Unity/Assets/Scripts/ExtraAbilities/Cannonscript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannonscript : MonoBehaviour {
+
+	Animator cannonanim;
+	//emitter for bullets
+	public GameObject emitter;
+	//cannon bullet
+	public GameObject cannonbullet;
+	//set bool
+	public bool animon;
+
+
+	// Use this for initialization
+	void Start () {
+		animon  = false;
+
+	}
+
+	void OnEnable()
+
+	{
+		cannonanim = GetComponent<Animator>();
+
+		cannonanim.SetTrigger("active");
+
+		StartCoroutine(Activecannon());
+	}
+
+	IEnumerator Activecannon()
+
+	{
+		animon = true;
+		yield return new WaitForSeconds(.5f);
+
+		cannonattack();
+
+		yield return new WaitForSeconds(5f);
+		animon = false;
+		cannonanim.SetTrigger("stop");
+	}
+
+	void cannonattack()
+
+	{
+	
+		if(animon){
+		Instantiate(cannonbullet,emitter.transform.position,Quaternion.identity);
+		}
+		StartCoroutine(Waiter());
+	}
+
+	IEnumerator Waiter()
+	{
+		yield return new WaitForSeconds(.3f);
+		cannonattack();
+
+	}
+
+	void Update () {
+	
+	}
+}
